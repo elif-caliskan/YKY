ba94e20021508fc9772dd2eb4f71e33da78b8616
Elif Çalışkan <>
2018-08-10 17:36:05 +0300

    book classı açıldı adaptor oluşturuldu.

---
+++ app/src/main/java/Book.java
@@ -0,0 +1,9 @@
+import android.widget.ArrayAdapter;
+
+public class ListItem {
+    String bookName;
+    String bookURL;
+}
+public class MyAdapter extends ArrayAdapter<String> {
+    // do some work
+}
