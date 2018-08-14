91e0c53c4a15a6ce03fd87308fd7adec2caa5e10
Elif Çalışkan <>
2018-08-14 11:37:31 +0300

    sorun düzeldi fragment yerine activity kullanıyorum

--- app/src/main/java/com/example/elifcaliskan/yky/ListActivity.java
+++ app/src/main/java/com/example/elifcaliskan/yky/ListActivity.java
@@ -2,37 +2,30 @@ package com.example.elifcaliskan.yky;
 
 
 import android.annotation.SuppressLint;
-import android.app.Fragment;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ListView;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 @SuppressLint("ValidFragment")
-public class BookFragment extends Fragment {
+public class ListActivity extends AppCompatActivity {
     BookAdapter adapter;
     String url;
     ArrayList<String> bookUrls= new ArrayList<String>();
     ArrayList<String> bookNames=new ArrayList<String>();
 
-    public static BookFragment newInstance(String url){
-
-        BookFragment fragment = new BookFragment();
-        fragment.url=url;
-        if(url!=null)
-            Log.i("buraya",url);
-        return fragment;
-
-    }
 
     public class DownloadTask extends AsyncTask<String,Void,String>{
 
@@ -74,67 +67,48 @@ public class BookFragment extends Fragment {
         }
     }
 
-    public BookFragment() {
-    }
-
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
 
-        /*View rootView = inflater.inflate(R.layout.books_list, container, false);
-
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.books_list);
+        Intent intent = getIntent();
+        url = intent.getStringExtra("url");
+        Log.i("gelen url", url);
         final ArrayList<Book> books = new ArrayList<Book>();
 
         DownloadTask task = new DownloadTask();
         String result;
-        Log.i("nerede","3");
+        Log.i("nerede", "3");
         try {
             result = task.execute(url).get();
-            Log.i("Contents of URL",result);
+            Log.i("Contents of URL", result);
             String[] splitResult1 = result.split("YKY Kitapları\">100 Temel Eserde YKY Kitapları</a></li>");
             String[] splitResult = splitResult1[1].split("<div class=\"footer-container\">");
 
-            Pattern p= Pattern.compile("src=\"(.*?)\"");
-            Matcher m=p.matcher(splitResult[0]);
+            Pattern p = Pattern.compile("src=\"(.*?)\"");
+            Matcher m = p.matcher(splitResult[0]);
 
-            while(m.find()){
+            while (m.find()) {
                 bookUrls.add(m.group(1));
             }
-            p= Pattern.compile("alt=\"(.*?)\"");
-            m=p.matcher(splitResult[0]);
+            p = Pattern.compile("alt=\"(.*?)\"");
+            m = p.matcher(splitResult[0]);
 
-            while(m.find()){
+            while (m.find()) {
                 bookNames.add(m.group(1));
             }
-            for(int i=0;i<bookNames.size();i++){
-                books.add(new Book(bookNames.get(i),bookUrls.get(i)));
+            for (int i = 0; i < bookNames.size(); i++) {
+                books.add(new Book(bookNames.get(i), bookUrls.get(i)));
             }
 
-        } catch (Exception e){
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
             e.printStackTrace();
         }
-
-        adapter = new BookAdapter(getActivity(), books, R.color.darkBlue);
-       // ListView listView = (ListView) rootView.findViewById(R.id.book_list);
-
-        //listView.setAdapter(adapter);
-
-
-        Log.i("nerede","1");*/
-        return inflater.inflate(R.layout.books_list, container, false);
+        adapter = new BookAdapter(this, books, R.color.darkBlue);
+        ListView listView = (ListView) findViewById(R.id.book_list);
+        listView.setAdapter(adapter);
     }
-    /*@Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        /*ListView lv = (ListView) view.findViewById(R.id.book_list);
-        Log.i("nerede","2");
-        lv.setAdapter(adapter);
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Book word;
-            }
-        });
-    }*/
 }
--- app/src/main/java/com/example/elifcaliskan/yky/MainActivity.java
+++ app/src/main/java/com/example/elifcaliskan/yky/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.elifcaliskan.yky;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,17 +13,11 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<String> categories = new ArrayList<String>();
-    private void loadFragment(Fragment fragment) {
-        // create a FragmentManager
-        FragmentManager fm = getFragmentManager();
-        // create a FragmentTransaction to begin the transaction and replace the Fragment
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        // replace the FrameLayout with new Fragment
-        fragmentTransaction.replace(R.id.book_list, fragment);
-        fragmentTransaction.commit(); // save the changes
-    }
+    String[] pos = {"a-z","","tekrar-basimlar","yeni-cikanlar","cok-satanlar","100-temel-eserde-yky-kitaplari"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         categories.add("A-Z");
@@ -36,67 +27,17 @@ public class MainActivity extends AppCompatActivity {
         categories.add("ÇOK SATANLAR");
         categories.add("100 TEMEL ESERDE YKY KİTAPLARI");
 
-
-        // Find the view pager that will allow the user to swipe between fragments
-        //ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-
-        // Create an adapter that knows which fragment should be shown on each page
-       // SimpleFragmentPageAdapter adapter = new SimpleFragmentPageAdapter(getSupportFragmentManager());
-
-        // Set the adapter onto the view pager
-       // viewPager.setAdapter(adapter);
-
-        /*TabLayout tabLayout = (TabLayout) findViewById(R.id.slidingtabs);
-        tabLayout.setupWithViewPager(viewPager);*/
-
-
-        ListView listView=(ListView)findViewById(R.id.myListView);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,categories);
+        ListView listView = (ListView) findViewById(R.id.list);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, categories);
         listView.setAdapter(arrayAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String url="http://kitap.ykykultur.com.tr/kitaplar/";
-                if(position==0){
-                    url=url+"a-z";
-                    loadFragment(BookFragment.newInstance(url));
-                }
-                else if(position==1){
-
-                    loadFragment(BookFragment.newInstance(url));
-                }
-                else if(position==2){
-                    url=url+"tekrar-basimlar";
-                    loadFragment(BookFragment.newInstance(url));
-                    Log.i("uyarı","2ye bastık");
-                }
-                else if(position==3){
-                    url=url+"yeni-cikanlar";
-                    loadFragment(BookFragment.newInstance(url));
-                }
-                else if(position==4){
-                    url=url+"cok-satanlar";
-                    loadFragment(BookFragment.newInstance(url));
-                }
-                else {
-                    url=url+"100-temel-eserde-yky-kitaplari";
-                    loadFragment(BookFragment.newInstance(url));
-
-                }
-                /*fragmentTransaction.add(R.id.bookFragment, f);
-                fragmentTransaction.commit();*/
-                setContentView(R.layout.books_list);
-
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                intent.putExtra("url","http://kitap.ykykultur.com.tr/kitaplar/"+pos[position]);
+                startActivity(intent);
             }
         });
-
-
-
     }
-
-
-
-
-
 }
