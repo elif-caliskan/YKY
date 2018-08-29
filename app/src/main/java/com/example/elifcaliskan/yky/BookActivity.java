909fe689f84cd05ec9564f2be4efdac05bad3159
Elif Çalışkan <elif.caliskan33098@gmail.com>
2018-08-30 00:40:33 +0300

    tab değişti çalışıyor ama farklı fragment yok bekleme görüntüs yok

--- app/src/main/java/com/example/elifcaliskan/yky/BookActivity.java
+++ app/src/main/java/com/example/elifcaliskan/yky/BookActivity.java
@@ -1,203 +1,43 @@
 package com.example.elifcaliskan.yky;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class BookActivity extends AppCompatActivity{
-    public class ImageDownloader extends AsyncTask<String, Void, Bitmap>{
-
-        @Override
-        protected Bitmap doInBackground(String... urls) {
-            try {
-                URL url=new URL(urls[0]);
-                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-                connection.connect();
-                InputStream inputStream = connection.getInputStream();
-                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
-                return myBitmap;
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-    }
-    public Bitmap downloadImage(View view, String url){
-        BookActivity.ImageDownloader task = new BookActivity.ImageDownloader();
-        Bitmap myImage;
-        try {
-            myImage = task.execute(url).get();
-            return myImage;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        return null;
-
-    }
-    String bookName;
-    String imageUrl;
-    String about;
-    String author;
-    String bookUrl;
-
-    public Map<String,String> letterMap=new HashMap<String, String>();
-    public String converter(String word){
-        while(word!=null&&word.contains("&")){
-            int index=word.indexOf("&");
-            if(word.contains(";")) {
-                String code = word.substring(index, word.indexOf(";")+1);
-                if(letterMap.containsKey(code)) {
-                    word = word.replace(code, letterMap.get(code));
-                }
-            }
-            else{
-                break;
-            }
-        }
-        return word;
-    }
-
-    public class DownloadTask extends AsyncTask<String,Void,String> {
-
-        @Override
-        protected String doInBackground(String... urls) {
-            letterMap.put("&#305;","ı");
-            letterMap.put("&#231;","ç");
-            letterMap.put("&#351;","ş");
-            letterMap.put("&#246;","ö");
-            letterMap.put("&#252;","ü");
-            letterMap.put("&#287;","ğ");
-            letterMap.put("&#304;","I");
-            letterMap.put("&#199;","Ç");
-            letterMap.put("&#350;","Ş");
-            letterMap.put("&#214;","Ö");
-            letterMap.put("&#220;","Ü");
-            letterMap.put("&#286;","Ğ");
-            letterMap.put("&#226;","â");
-            letterMap.put("&#39;","'");
-            letterMap.put("&Ccedil;","Ç");
-            letterMap.put("&ccedil;","ç");
-            letterMap.put("&Ouml;","Ö");
-            letterMap.put("&ouml;","ö");
-            letterMap.put("&Uuml;","Ü");
-            letterMap.put("&uuml;","ü");
-            letterMap.put("&Agrave;","À");
-            letterMap.put("&agrave;","à");
-            letterMap.put("&Acirc;","Â");
-            letterMap.put("&acirc;","â");
-            letterMap.put("&Egrave;","È");
-            letterMap.put("&egrave;;","è");
-            letterMap.put("&Eacute;","É");
-            letterMap.put("&eacute;","é");
-            letterMap.put("&Ecirc;","Ê");
-            letterMap.put("&ecirc;","ê");
-            letterMap.put("&ldquo;","“");
-            letterMap.put("&rsquo;","'");
-            letterMap.put("&rdquo;","”");
-
-            String result = "";
-            URL url;
-            HttpURLConnection urlConnection = null;
-
-            try {
-
-                url = new URL(urls[0]);
-
-                urlConnection = (HttpURLConnection)url.openConnection();
-
-                InputStream in =  urlConnection.getInputStream();
-
-                InputStreamReader reader = new InputStreamReader(in);
-
-                int data = reader.read();
-
-                while(data !=-1){
-
-                    char current = (char)data;
-
-                    result += current;
-
-                    data = reader.read();
-                }
-                return result;
-            }
-            catch (Exception e){
-                e.printStackTrace();
-            }
-            return null;
-        }
-    }
+public class BookActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.about_book);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.book_pager);
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        BookCategoryAdapter adapter = new BookCategoryAdapter(this, getSupportFragmentManager());
         Intent intent = getIntent();
-        imageUrl = intent.getStringExtra("imageUrl");
-        author = intent.getStringExtra("authorName");
-        bookName = intent.getStringExtra("bookName");
-        bookUrl = intent.getStringExtra("bookUrl");
+        adapter.setAuthor(intent.getStringExtra("authorName"));
+        adapter.setBookUrl(intent.getStringExtra("bookUrl"));
+        adapter.setBookName(intent.getStringExtra("bookName"));
+        adapter.setImageUrl(intent.getStringExtra("imageUrl"));
 
-        BookActivity.DownloadTask task = new BookActivity.DownloadTask();
-        String result;
-        try {
-            result = task.execute(bookUrl).get();
-            Log.i("Contents of URL", result);
-            String[] splitResult1 = result.split("<div id=\"tab1\" class=\"tab-content clearfix selected\">");
-            String[] splitResult = splitResult1[1].split("<div id=\"tab3\" class=\"tab-content clearfix\">");
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
 
-            Pattern p = Pattern.compile("<p>(.*?)</p>");
-            Matcher m = p.matcher(splitResult[0]);
-            String aboutBook="\t";
+        // Find the tab layout that shows the tabs
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
 
-            while(m.find()) {
-                aboutBook += m.group(1);
-                aboutBook+="\n\n\t";
-            }
+        tabLayout.setupWithViewPager(viewPager);
 
-            about=converter(aboutBook);
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        TextView textView = (TextView)findViewById(R.id.book_name);
-        textView.setText(bookName);
-        textView=(TextView)findViewById(R.id.book_author);
-        textView.setText(author);
-        textView=(TextView)findViewById(R.id.book_about);
-        textView.setText(about);
-        textView=findViewById(R.id.about);
-        textView.setText("Hakkında:");
-        ImageView iconView=(ImageView) findViewById(R.id.book_cover);
-        iconView.setImageBitmap(downloadImage(iconView,imageUrl));
-        iconView.setVisibility(View.VISIBLE);
 
     }
-
 }
---
+++ app/src/main/java/com/example/elifcaliskan/yky/BookCategoryAdapter.java
@@ -0,0 +1,106 @@
+package com.example.elifcaliskan.yky;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
+
+public class BookCategoryAdapter extends FragmentPagerAdapter {
+
+    private Context mContext;
+    private String imageUrl;
+    private String bookUrl;
+    private String bookName;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getBookUrl() {
+        return bookUrl;
+    }
+
+    public void setBookUrl(String bookUrl) {
+        this.bookUrl = bookUrl;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    private String author;
+
+    public BookCategoryAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        if (position == 0) {
+            AboutFragment fragment = new AboutFragment();
+            fragment.bookName=bookName;
+            fragment.bookUrl=bookUrl;
+            fragment.author=author;
+            fragment.imageUrl=imageUrl;
+            return fragment;
+        } else if (position == 1) {
+            AboutFragment fragment = new AboutFragment();
+            fragment.bookName=bookName;
+            fragment.bookUrl=bookUrl;
+            fragment.author=author;
+            fragment.imageUrl=imageUrl;
+            return fragment;
+        } else if (position == 2) {
+            AboutFragment fragment = new AboutFragment();
+            fragment.bookName=bookName;
+            fragment.bookUrl=bookUrl;
+            fragment.author=author;
+            fragment.imageUrl=imageUrl;
+            return fragment;
+        } else if (position == 3) {
+            AboutFragment fragment = new AboutFragment();
+            fragment.bookName=bookName;
+            fragment.bookUrl=bookUrl;
+            fragment.author=author;
+            fragment.imageUrl=imageUrl;
+            return fragment;
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.about_book);
+        } else if (position == 1) {
+            return mContext.getString(R.string.book_info);
+        } else if (position == 2) {
+            return mContext.getString(R.string.tadımlık);
+        } else {
+            return mContext.getString(R.string.comment);
+        }
+    }
+}
